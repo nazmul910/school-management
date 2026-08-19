4bc2088cb891d02e314b6be2d9114dce35d91b33
nazmul910 <islamnasir910@gmail.com>
2026-08-19 22:54:51 +0600

    fix: reorder middleware usage and update TypeScript configuration for improved clarity

--- school-backend/src/app.ts
+++ school-backend/src/app.ts
@@ -12,12 +12,14 @@ app.use(cors());
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 app.use(cookieParser());
-app.use("/api/v1", router);
-
-app.use(globalErrorHandler);
-app.use(notFound);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("School Management System API is running");
 });
+
+app.use("/api/v1", router);
+
+app.use(notFound);          
+app.use(globalErrorHandler); 
+
 export default app;
